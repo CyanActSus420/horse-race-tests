2c5d90511e7b8e0c4af5d61bff715d4c8185b694
CyanActSus420 <111568989+CyanActSus420@users.noreply.github.com>
2025-04-29 17:48:49 +0200

    map changes + timer

--- scripts/State-Race.gd
+++ scripts/State-Race.gd
@@ -5,12 +5,37 @@ extends Node2D
 @onready var time_scale_slider: HSlider = $HUD/TimeScale
 @onready var time_scale_label: Label = $HUD/TimeScale/Label
 
+@onready var time_label: Label = $HUD/TimeLabel
+
+# i know, i know its a shitty ass fix
+var nanoseconds:int = 0
+var tensofseconds:int = 0
+var seconds:int = 0
+var minutes:int = 0
+
 func _process(delta: float) -> void:
 	time_scale_label.text = "TimeScale: x%s" % Engine.time_scale
 	Engine.time_scale = time_scale_slider.value
 	
 	if Input.is_action_just_pressed("hidehud"):
 		hud.visible = !hud.visible
+	
+	time_label.text = "%s:%s%s:%s" % [minutes, tensofseconds, seconds, nanoseconds]
 
 func _on_timer_timeout() -> void:
 	GameHandler.game_started.emit()
+	$NanosecondTimer.start()
+
+func _on_nanosecond_timer_timeout() -> void:
+	nanoseconds += 1
+	if nanoseconds == 10:
+		nanoseconds = 0
+		seconds += 1
+		
+	if seconds == 10:
+		seconds = 0
+		tensofseconds += 1
+		
+	if tensofseconds == 6:
+		tensofseconds = 0
+		minutes += 1
