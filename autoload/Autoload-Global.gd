57ba31391e67f5c54a63a49b623c2d0172aaf491
CyanActSus420 <111568989+CyanActSus420@users.noreply.github.com>
2025-04-29 17:17:10 +0200

    fullscreen shit

--- autoload/Autoload-Global.gd
+++ autoload/Autoload-Global.gd
@@ -5,5 +5,14 @@ extends Node
 var scene_to_load = "res://scenes/Race.tscn"
 var loading_screen = preload("res://scenes/Loading.tscn")
 
+var fullscreen = false
+
 func _process(delta: float) -> void:
 	fps_label.text = "FPS: %s" % Engine.get_frames_per_second()
+	
+	if Input.is_action_just_pressed("fullscreen"):
+		if fullscreen:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		fullscreen = !fullscreen
