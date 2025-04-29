945720adec2c94f8e6b3c7b8db5bce8b778fff91
CyanActSus420 <111568989+CyanActSus420@users.noreply.github.com>
2025-04-29 17:16:58 +0200

    loading "hints"

--- scripts/Loading.gd
+++ scripts/Loading.gd
@@ -4,8 +4,24 @@ extends Node2D
 @onready var hint_label: Label = $HintLabel
 @onready var horse: Sprite2D = $horse
 
+var loading_texts = [
+	"haha made you look",
+	"who up racing their horse rn",
+	"this is a hint",
+	"check steam",
+	"don't forget to like and subscribe!",
+	"horses neigh",
+	"no horses were hurt during this race",
+	"place your bets while you're waiting",
+	"horsey horsing horse",
+	"hold shift to sprint",
+	"did you know the horses have names? me neither",
+	"you're reading this because it's loading"
+]
+
 
 func _ready():
+	hint_label.text = loading_texts[randi_range(0, loading_texts.size() - 1)]
 	ResourceLoader.load_threaded_request(Global.scene_to_load)
 
 func _process(delta: float) -> void:
